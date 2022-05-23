cf83d7600ca86cb8b64d5149e6531b91e2106ab8
aust1inn <austinotieno364@gmail.com>
2022-05-23 17:11:34 +0300

    sets up the navbar

--- news/views.py
+++ news/views.py
@@ -1,26 +1,20 @@
-from django.http import HttpResponse
 from django.http import HttpResponse, Http404
+from django.shortcuts import render,redirect
 import datetime as dt
 
 
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')
 
 def news_of_day(request):
     date = dt.date.today()
     
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
     day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1> News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    
+    return render(request,'all-news/today-news.html', {"date":date})
 
 def convert_dates(dates):
 
@@ -41,13 +35,9 @@ def past_days_news(request,past_date):
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-news/past-news.html', {"date":date})
