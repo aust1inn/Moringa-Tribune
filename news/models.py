f78e59e1ba6aa18024ccb9c38d753f102fdeaa96
aust1inn <austinotieno364@gmail.com>
2022-05-25 17:52:08 +0300

    configures image uploading option

--- news/models.py
+++ news/models.py
@@ -26,7 +26,8 @@ class Article(models.Model):
     post = models.TextField()
     editor = models.ForeignKey(Editor ,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
-    pub_date = models.DateTimeField(auto_now_add=True)      
+    pub_date = models.DateTimeField(auto_now_add=True) 
+    article_image=models.ImageField(upload_to = 'articles/',default='default.jpg')     
 
     @classmethod
     def todays_news(cls):
@@ -39,3 +40,7 @@ class Article(models.Model):
         news = cls.objects.filter(pub_date__date = date)
         return news
     
+    @classmethod
+    def search_by_title(cls,search_term):
+        news = cls.objects.filter(title__icontains=search_term)
+        return news
