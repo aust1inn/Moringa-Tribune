6e1c0814b09242ba109e2cf40f1f1d9099e58071
aust1inn <austinotieno364@gmail.com>
2022-05-26 09:18:31 +0300

    deployment changes

--- tribune/settings.py
+++ tribune/settings.py
@@ -186,3 +186,6 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
