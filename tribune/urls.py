1ba2d20b250eb6ae0aef4d2e547569547af60c76
aust1inn <austinotieno364@gmail.com>
2022-06-01 22:39:14 +0300

    adds links for registration and login to navbar

--- tribune/urls.py
+++ tribune/urls.py
@@ -18,11 +18,15 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.urls import path
 from users import views as user_views
+from django.contrib.auth import views as auth_views
+
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('news.urls')),
     path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
 ]
